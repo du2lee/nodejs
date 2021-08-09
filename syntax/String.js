7bcb19f0a4729d9f9b3e36fc360be10571aa8ad1
duhui <dh151446@gmail.com>
2021-08-10 08:51:58 +0900

    add syntax

---
+++ syntax/String.js
@@ -0,0 +1,2 @@
+console.log('1' + '1')
+console.log('There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don'.length);
